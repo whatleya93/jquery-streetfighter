f77a95950c0ab991a49b833cfa046b73fd393e00
whatleya93 <whatleya93@live.com>
2015-07-10 00:37:18 -0500

    Added hadouken sound.

--- js/app.js
+++ js/app.js
@@ -8,14 +8,26 @@ $(document).ready(function() {
 		$('.ryu-still').show();
 	})
 	.mousedown(function() {
-		//play hadouken sound
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		//animate hadouken across the screen
+		$('.hadouken').finish().show().animate(
+			{'left': '1120px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '620px')
+			}
+		);
 	})
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
 });
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}

5634a4d2ec78561b16d70444e8f464623d74dc22
whatleya93 <whatleya93@live.com>
2015-07-10 00:21:14 -0500

    Finished ryu animations for ready and throwing.

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	$('.ryu').mouseenter(function() {
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	.mousedown(function() {
+		//play hadouken sound
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show();
+		//animate hadouken across the screen
+	})
+	.mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	});
+});
